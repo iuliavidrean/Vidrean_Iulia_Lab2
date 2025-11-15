4cdef63af1c8bb635f8eefb9455f1f0377be3b17
IULIA VIDREAN <iulia.vidrean1@stud.ubbcluj.ro>
2025-11-15 11:55:40 +0200

    Laborator 5

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Vidrean_Iulia_Lab2.Data;
 using Vidrean_Iulia_Lab2.Models;
 
--- Pages/Books/Edit.cshtml.cs
+++ Pages/Books/Edit.cshtml.cs
@@ -2,10 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using Vidrean_Iulia_Lab2.Data;
 using Vidrean_Iulia_Lab2.Models;
 
